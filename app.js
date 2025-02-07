26d77b1fd96a74e78600107f0042dd083f21c351
gleissonS89 <gsrds1989@gmail.com>
2025-02-07 17:09:30 -0300

    Projeto inicial

---
+++ app.js
@@ -0,0 +1,28 @@
+let campo = document.querySelector('input');
+let listaDeParticipantes = [];
+
+
+function adicionarAmigo() {
+    if (campo.value === '') {
+        alert('Digite um nome válido!'); 
+        return;
+    }
+
+    listaDeParticipantes.push(campo.value);
+        let lista = document.getElementById('listaAmigos');
+        const listaAmigos = document.createElement('ul');
+        listaAmigos.textContent = campo.value;
+        lista.appendChild(listaAmigos);
+        campo.value = '';
+        //console.log(listaDeParticipantes);
+        
+    }
+
+function sortearAmigo() {
+    let sortear = listaDeParticipantes[Math.floor(Math.random() * listaDeParticipantes.length)];
+    let sorteado = document.getElementById('resultado');
+    let lista = document.getElementById('listaAmigos');
+            lista.style.display = 'none';
+    sorteado.innerHTML = `O amigo secreto é: ${sortear}`;
+    //console.log(sortear);
+}
